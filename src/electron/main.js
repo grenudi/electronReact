a9a8811dff15ca1160468799f603dd96ced1d39b
r.shylo <r.shylo@infostroy.com.ua>
2020-05-07 20:51:15 +0300

    init

---
+++ src/electron/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  mainWindow.loadURL(
+    process.env.NODE_ENV == "dev"
+      ? "http://localhost:8080"
+      : `file://${__dirname}/../../dist/index.html`
+  );
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
